774bcefc729c98004316e0d038bd55d31069830d
yahuaiii0923 <D00275358@student.dkit.ie>
2025-02-11 23:13:28 +0000

    database connection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -16,16 +16,6 @@ public class Main {
         try (Connection conn = DriverManager.getConnection(url + dbName, userName, password)) {
             System.out.println("SUCCESS! Connected to the MySQL Database.");
 
-            // Run an SQL Query
-            String query = "SELECT * FROM expenses";
-            try (Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(query)) {
-                System.out.println("Expenses in the database:");
-                while (rs.next()) {
-                    System.out.println("ID: " + rs.getInt("expenseID") + ", Title: " + rs.getString("title") +
-                            ", Category: " + rs.getString("category") + ", Amount: €" + rs.getDouble("amount") + ", Date: " + rs.getDate("dateIncurred"));
-                }
-            }
-
             System.out.println("Closing connection...");
         } catch (SQLException ex) {
             System.out.println("Failed to connect! Make sure the MySQL server is running and the database exists.");
