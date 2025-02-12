99185ae3e30b4655718d5ad5874c276a6b1e2fb9
yahuaiii0923 <D00275358@student.dkit.ie>
2025-02-12 19:43:30 +0000

    expense dto

---
+++ src/main/java/org/example/DTOs/Expenses.java
@@ -0,0 +1,79 @@
+package org.example.DTOs;
+
+import java.util.Date;
+
+public class Expenses {
+    private int expenseID;
+    private String title;
+    private String category;
+    private double amount;
+    private Date dateIncurred;
+
+    public Expenses(int expenseID, String title, String category, double amount, Date dateIncurred) {
+        this.expenseID = expenseID;
+        this.title = title;
+        this.category = category;
+        this.amount = amount;
+        this.dateIncurred = dateIncurred;
+    }
+
+    public Expenses(String title, String category, double amount, Date dateIncurred) {
+        this.title = title;
+        this.category = category;
+        this.amount = amount;
+        this.dateIncurred = dateIncurred;
+    }
+
+    public Expenses(){}
+
+    public int getExpenseID() {
+        return expenseID;
+    }
+
+    public void setExpenseID(int expenseID) {
+        this.expenseID = expenseID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDateIncurred() {
+        return dateIncurred;
+    }
+
+    public void setDateIncurred(Date dateIncurred) {
+        this.dateIncurred = dateIncurred;
+    }
+
+    @Override
+    public String toString() {
+        return "Expenses{" +
+                "expenseID=" + expenseID +
+                ", title='" + title + '\'' +
+                ", category='" + category + '\'' +
+                ", amount=" + amount +
+                ", dateIncurred=" + dateIncurred +
+                '}';
+    }
+}
