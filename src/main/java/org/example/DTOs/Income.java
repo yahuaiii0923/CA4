f37b633e8956cd1b2bfc8a2a61831354c3531aec
yahuaiii0923 <D00275358@student.dkit.ie>
2025-02-12 19:57:35 +0000

    income dto

--- src/main/java/org/example/DTOs/Income.java
+++ src/main/java/org/example/DTOs/Income.java
@@ -1,4 +1,68 @@
 package org.example.DTOs;
 
+import java.util.Date;
+
 public class Income {
+    private int incomeID;
+    private String title;
+    private double amount;
+    private Date dateEarned;
+
+
+    public Income(int incomeID, String title, double amount, Date dateEarned){
+        this.incomeID = incomeID;
+        this.title = title;
+        this.amount = amount;
+        this.dateEarned = dateEarned;
+    }
+
+    public Income(String title, String category, double amount, Date dateEarned){
+        this.title = title;
+        this.amount = amount;
+        this.dateEarned = dateEarned;
+    }
+
+    public Income(){}
+
+    public int getIncomeID() {
+        return incomeID;
+    }
+
+    public void setIncomeID(int incomeID) {
+        this.incomeID = incomeID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public Date getDateEarned() {
+        return dateEarned;
+    }
+
+    public void setDateEarned(Date dateEarned) {
+        this.dateEarned = dateEarned;
+    }
+
+    @Override
+    public String toString() {
+        return "Income{" +
+                "incomeID=" + incomeID +
+                ", title='" + title + '\'' +
+                ", amount=" + amount +
+                ", dateEarned=" + dateEarned +
+                '}';
+    }
 }
