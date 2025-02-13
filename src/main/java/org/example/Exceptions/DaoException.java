0177f036fe2da4327f79f47696f5c7ae46ddbfb3
yahuaiii0923 <D00275358@student.dkit.ie>
2025-02-13 00:15:18 +0000

    created DAOs

---
+++ src/main/java/org/example/Exceptions/DaoException.java
@@ -0,0 +1,18 @@
+package org.example.Exceptions;
+
+import java.sql.SQLException;
+
+
+public class DaoException extends SQLException
+{
+    public DaoException()
+    {
+        // not used
+    }
+
+    public DaoException(String aMessage)
+    {
+        super(aMessage);
+    }
+}
+
